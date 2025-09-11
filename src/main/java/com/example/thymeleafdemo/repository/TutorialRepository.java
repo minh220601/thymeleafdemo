6a7cb3a008f818e74ca2fe1420836f726c3db880
anhminh2206 <zzaquarius220@gmail.com>
2025-09-11 10:47:49 +0700

    add file TutorialRepository.java

---
+++ src/main/java/com/example/thymeleafdemo/repository/TutorialRepository.java
@@ -0,0 +1,21 @@
+package com.example.thymeleafdemo.repository;
+
+import com.example.thymeleafdemo.entity.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Integer> {
+    List<Tutorial> findByTitleContainingIgnoreCase(String keyword);
+
+    @Query("UPDATE Tutorial t SET t.published = :published WHERE t.id = :id")
+    @Modifying
+    public void updatePublishedStatus(Integer id, boolean published);
+}
